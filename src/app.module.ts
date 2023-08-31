b0e67a8b2dc515ad8f67e652fea04a470edc8fb9
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-08-31 11:29:28 -0400

    Config de postgres, typeorm y definicion  de entity product

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), 
+            TypeOrmModule.forRoot({
+              type: 'postgres',
+              host: process.env.DB_HOST,
+              port: parseInt(process.env.DB_PORT),
+              username: process.env.DB_USERNAME,
+              password: process.env.DB_PASSWORD,
+              database: process.env.DB_NAME,
+              autoLoadEntities: true,
+              synchronize: true,
+            }), ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
