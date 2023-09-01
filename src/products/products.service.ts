381ebd7fd532b4f270ad295d69722aae92bca462
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-09-01 11:14:10 -0400

    Get and Delete Products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -27,20 +27,44 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    try{
+      return await this.productRepository.find({});
+    }
+    catch(error){
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error retrieving products');
+    }
+    
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  //metodo findOne con product repository
+  async findOne(id: string) {
+    try{
+      const product = await this.productRepository.findOneBy({id});
+      if(!product)
+        throw new NotFoundException(`Product #${id} not found`);
+      return product;
+    }
+    catch(error){
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error retrieving product')
+    }
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    try{
+      const product = await this.findOne(id);
+      await this.productRepository.remove(product);
+    }
+    catch(error){
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error deleting product');
+    }
   }
 
   private handleDBExceptions(error: any){
