66521bbdff4099c75d5394656a1bff7b6844d059
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-12-07 17:31:24 -0400

    Email changed to id on the payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
       delete user.password;
       return {
         ...user,
-        token: this.getJwtToken({email: user.email})
+        token: this.getJwtToken({id: user.id})
       }
     }
     catch(error){
@@ -49,7 +49,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where : {email},
-      select : {email: true, password: true}
+      select : {email: true, password: true, id:true}
     });
 
     if(!user){
@@ -62,7 +62,7 @@ export class AuthService {
     
     return {
       ...user,
-      token: this.getJwtToken({email: user.email})
+      token: this.getJwtToken({id: user.id})
     }
   }
 
